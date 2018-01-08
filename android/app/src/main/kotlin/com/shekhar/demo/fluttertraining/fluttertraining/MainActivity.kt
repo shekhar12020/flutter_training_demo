4f6eaae1ddbdbea5d4dc6196ed7b8e417a9927e8
Shekhar Sahu <chandrashekhar@joshsoftware.com>
2018-01-08 22:44:09 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/shekhar/demo/fluttertraining/fluttertraining/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.shekhar.demo.fluttertraining.fluttertraining
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity(): FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
